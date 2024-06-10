6f5f2e21ccfd4bc1b44b3d6c6a27e26027ac9fa0
Chris <hereokay88@gmail.com>
2024-06-10 20:34:12 +0900

    feat: finalVote

--- main.js
+++ main.js
@@ -28,12 +28,18 @@ function calcStudentSaltHash(studentId, salt){
 const PORT = 4000;
 const PROVIDER_URL = process.env.PROVIDER_URL; // e.g., Infura URL or Alchemy URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY; // Private key of the sender account
-const CONTRACT_ADDRESS = process.env.VOTINGBOX_CONTRACT_ADDRESS; // votingBox
-const CONTRACT_ABI = [
+const VOTINGBOX_ADDRESS = process.env.VOTINGBOX_ADDRESS; // votingBox
+const VOTINGBOX_ABI = [
   'function studentSaltTable(uint256) public view returns (uint256)',
   'function setSalt(uint256[] memory studentNumberList, uint256[] memory saltHash) external',
   'function registVoter(address voterAddress, uint hashValue) external'
 ];
+
+
+const TORNADO_ADDRESS = process.env.TORNADO_ADDRESS;
+const TORNADO_ABI = [
+  
+]
 
 // Initialize Express app
 const app = express();
@@ -44,15 +50,47 @@ app.use(cors());
 const provider = new ethers.JsonRpcProvider(PROVIDER_URL);
 const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
 
-// Parse ABI and create contract instance
-const abi = parseAbi(CONTRACT_ABI);
-const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, wallet);
+
+
+// Define route to handle GET requests
+app.post('/finalVote', async (req, res) => {
+  try {
+    // Call the contract function (example: calling setSalt function)
+     // Parse ABI and create contract instance
+    const abi = parseAbi(TORNADO_ABI);
+    const contract = new ethers.Contract(TORNADO_ADDRESS, abi, wallet);
+
+    const { callInputs, tokenAddress, candidateAddress } = req.body;
+
+    // const tx = await tornado.connect(layer).withdraw(...callInputs, token.target, candidate.address);
+
+    const txResponse = await contract.withdraw(...callInputs, tokenAddress, candidateAddress);
+
+    // Wait for the transaction to be mined
+    const receipt = await txResponse.wait();
+
+    // Send response
+    res.json({
+      message: 'Contract function called successfully!',
+      txHash: receipt.transactionHash,
+      receipt: receipt
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to call contract function' });
+  }
+});
 
 // Define route to handle GET requests
 app.get('/allocateAddress', async (req, res) => {
   try {
     // Call the contract function (example: calling setSalt function)
-    
+  
+    // Parse ABI and create contract instance
+    const abi = parseAbi(VOTINGBOX_ABI);
+    const contract = new ethers.Contract(VOTINGBOX_ADDRESS, abi, wallet);
+
+
     const code = req.query.code;
 
 
@@ -81,5 +119,8 @@ app.get('/allocateAddress', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
+
 
 
